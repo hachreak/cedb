84dd1b7beca9b1c9f57c2f9b2bd7c31661c05e31
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-02-17 15:40:15 +0100

    cedb: show code on debug
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- src/cedb.erl
+++ src/cedb.erl
@@ -21,6 +21,7 @@ break(Module, Line) ->
 
 debug(Pid) ->
   {ok, Meta} = int:attached(Pid),
+  meta_response(),
   eval(go, Meta),
   int:continue(Pid),
   false.
@@ -30,7 +31,8 @@ eval(Cmd, Meta) ->
   case lists:member(Cmd, [finish, next, step, skip, stop,
                           messages, timeout]) of
     true ->
-      int:meta(Meta, Cmd);
+      int:meta(Meta, Cmd),
+      meta_response();
     false ->
       eval2(Cmd, Meta)
   end,
@@ -38,11 +40,51 @@ eval(Cmd, Meta) ->
 
 eval2(bindings, Meta) ->
   Ret = int:meta(Meta, bindings, nostack),
-  io:format("bind: ~p~n", [Ret]);
+  io:format("bind: ~p~n", [Ret]),
+  meta_response();
 eval2(_, _) -> ok.
 
 repl(Meta) ->
   eval(run(io:get_line("cedb> ")), Meta).
+
+meta_response() ->
+  receive
+    {_Pid, {attached, Module, Line, _Trace}} ->
+      show_line(Module, Line);
+    {_Pid, {break_at, Module, Line, _Column}} ->
+      show_line(Module, Line);
+    Msg -> io:format("MSG: ~p~n", [Msg])
+  end.
+
+show_line(Module, LineNumber) ->
+  Filename = int:file(Module),
+  {ok, File} = file:read_file(Filename),
+  Lines = re:split(File, "\n"),
+  {First, Last} = range_to_show(Lines, LineNumber),
+  LinesToShow = lists:sublist(Lines, First, Last - First + 1),
+  Seq = lists:seq(First, Last),
+  lists:foreach(fun({Number, Line}) ->
+      String = binary_to_list(Line),
+      Color = case Number =:= LineNumber of
+        true -> "C0C000";
+        false -> "606000"
+      end,
+      io:format("  ~p:\t~s~n", [Number, color:true(Color, String)])
+    end, lists:zip(Seq, LinesToShow)).
+
+range_to_show(Lines, LineNumber) ->
+  Length = length(Lines),
+  Begin = LineNumber - 5,
+  End = LineNumber + 5,
+  Last = case End > Length of
+    true -> Length;
+    false -> End
+  end,
+  First = case Begin < 0 of
+    true -> 0;
+    false -> Begin
+  end,
+  {First, Last}.
 
 run("\n") -> "";
 run(Expression) ->
